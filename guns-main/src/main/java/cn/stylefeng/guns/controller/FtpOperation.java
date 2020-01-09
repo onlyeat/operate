92f55287fd772e344b53c242618e05ea91cf7c5f
liuqianyi <195435179>
2020-01-09 17:53:10 +0800

    ftp模块

--- guns-main/src/main/java/cn/stylefeng/guns/controller/FtpOperation.java
+++ guns-main/src/main/java/cn/stylefeng/guns/controller/FtpOperation.java
@@ -15,69 +15,56 @@ import java.util.Date;
 
 /**
  * 类说明：文件上传下载工具类
- *
  */
 @Component
-public class FtpOperation{
-    public static final int imageCutSize=300;
-    private static final Logger log= LoggerFactory.getLogger(FtpOperation.class);
+public class FtpOperation {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FtpOperation.class);
 
-    @Value("${ftp.username}")
+    @Value("${ftp.erp.username}")
     private String userName;
 
-    @Value("${ftp.password}")
+    @Value("${ftp.erp.password}")
     private String passWord;
 
-    @Value("${ftp.host}")
+    @Value("${ftp.erp.hostname}")
     private String ip;
 
-    @Value("${ftp.port}")
+    @Value("${ftp.erp.port}")
     private int port;
 
-    @Value("${ftp.filepath}")
-    private String CURRENT_DIR;     // 文件存放的目录
+    @Value("${ftp.erp.path}")
+    private String currentDir;     // 文件存放的目录
 
-    public static final String DIRSPLIT="/";
-
-    public String getCURRENT_DIR() {
-        return CURRENT_DIR;
-    }
-
-
-    public void setCURRENT_DIR(String cURRENTDIR) {
-        CURRENT_DIR = cURRENTDIR;
-    }
+    public static final String DIRSPLIT = "/";
 
     // 下载的文件目录
-    private String DOWNLOAD_DIR;
+    private String downloadDir;
 
     // ftp客户端
     private FTPClient ftpClient = new FTPClient();
 
     /**
-     *
      * 功能：上传文件附件到文件服务器
+     *
      * @param buffIn:上传文件流
      * @param fileName：保存文件名称
      * @param needDelete：是否同时删除
      * @return
      * @throws IOException
      */
-    public boolean uploadToFtp(InputStream buffIn, String fileName,boolean needDelete)
-            throws FTPConnectionClosedException, IOException,Exception {
+    public boolean uploadToFtp(InputStream buffIn, String fileName, boolean needDelete)
+            throws FTPConnectionClosedException, IOException, Exception {
         boolean returnValue = false;
         // 上传文件
         try {
-
             // 建立连接
             connectToServer();
             // 设置传输二进制文件
             setFileType(FTP.BINARY_FILE_TYPE);
             int reply = ftpClient.getReplyCode();
-            if(!FTPReply.isPositiveCompletion(reply))
-            {
+            if (!FTPReply.isPositiveCompletion(reply)) {
                 ftpClient.disconnect();
-                throw new IOException("failed to connect to the FTP Server:"+ip);
+                throw new IOException("failed to connect to the FTP Server:" + ip);
             }
             ftpClient.enterLocalPassiveMode();
                /* if(StringUtils.checkStr(CURRENT_DIR)){
@@ -88,7 +75,7 @@ public class FtpOperation{
                 }*/
             // 上传文件到ftp
             returnValue = ftpClient.storeFile(fileName, buffIn);
-            if(needDelete){
+            if (needDelete) {
                 ftpClient.deleteFile(fileName);
             }
             // 输出操作结果信息
@@ -124,15 +111,15 @@ public class FtpOperation{
 
 
     /**
-     *
      * 功能：根据文件名称，下载文件流
+     *
      * @param filename
      * @return
      * @throws IOException
      */
-    public InputStream  downloadFile(String filename)
+    public InputStream downloadFile(String filename)
             throws IOException {
-        InputStream in=null;
+        InputStream in = null;
         try {
 
             // 建立连接
@@ -141,21 +128,20 @@ public class FtpOperation{
             // 设置传输二进制文件
             setFileType(FTP.BINARY_FILE_TYPE);
             int reply = ftpClient.getReplyCode();
-            if(!FTPReply.isPositiveCompletion(reply))
-            {
+            if (!FTPReply.isPositiveCompletion(reply)) {
                 ftpClient.disconnect();
-                throw new IOException("failed to connect to the FTP Server:"+ip);
+                throw new IOException("failed to connect to the FTP Server:" + ip);
             }
             ftpClient.changeWorkingDirectory(CURRENT_DIR);
 
             // ftp文件获取文件
-            in=ftpClient.retrieveFileStream(filename);
+            in = ftpClient.retrieveFileStream(filename);
 
         } catch (FTPConnectionClosedException e) {
             log.error("ftp连接被关闭！", e);
             throw e;
         } catch (Exception e) {
-            log.error("ERR : upload file "+ filename+ " from ftp : failed!", e);
+            log.error("ERR : upload file " + filename + " from ftp : failed!", e);
         }
         return in;
     }
@@ -180,8 +166,7 @@ public class FtpOperation{
     /**
      * 设置传输文件的类型[文本文件或者二进制文件]
      *
-     * @param fileType
-     *            --BINARY_FILE_TYPE、ASCII_FILE_TYPE
+     * @param fileType --BINARY_FILE_TYPE、ASCII_FILE_TYPE
      */
     private void setFileType(int fileType) {
         try {
@@ -192,7 +177,6 @@ public class FtpOperation{
     }
 
     /**
-     *
      * 功能：关闭连接
      */
     public void closeConnect() {
@@ -209,13 +193,13 @@ public class FtpOperation{
     /**
      * 连接到ftp服务器
      */
-    private void connectToServer() throws FTPConnectionClosedException,Exception {
+    private void connectToServer() throws FTPConnectionClosedException, Exception {
         if (!ftpClient.isConnected()) {
             int reply;
             try {
-                ftpClient=new FTPClient();
-                ftpClient.connect(ip,port);
-                ftpClient.login(userName,passWord);
+                ftpClient = new FTPClient();
+                ftpClient.connect(ip, port);
+                ftpClient.login(userName, passWord);
                 reply = ftpClient.getReplyCode();
 
                 if (!FTPReply.isPositiveCompletion(reply)) {
@@ -223,15 +207,16 @@ public class FtpOperation{
                     log.info("connectToServer FTP server refused connection.");
                 }
 
-            }catch(FTPConnectionClosedException ex){
-                log.error("服务器:IP："+ip+"没有连接数！there are too many connected users,please try later", ex);
+            } catch (FTPConnectionClosedException ex) {
+                log.error("服务器:IP：" + ip + "没有连接数！there are too many connected users,please try later", ex);
                 throw ex;
-            }catch (Exception e) {
-                log.error("登录ftp服务器【"+ip+"】失败", e);
+            } catch (Exception e) {
+                log.error("登录ftp服务器【" + ip + "】失败", e);
                 throw e;
             }
         }
     }
+
     // Check the path is exist; exist return true, else false.
     public boolean existDirectory(String path) throws IOException {
         boolean flag = false;
@@ -245,23 +230,27 @@ public class FtpOperation{
         }
         return flag;
     }
+
     /**
      * 创建FTP文件夹目录
+     *
      * @param pathName
      * @return
      * @throws IOException
      */
     public boolean createDirectory(String pathName) throws IOException {
-        boolean isSuccess=false;
-        try{
-            isSuccess=ftpClient.makeDirectory(pathName);
-        }catch(Exception e){
+        boolean isSuccess = false;
+        try {
+            isSuccess = ftpClient.makeDirectory(pathName);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return isSuccess;
     }
+
     /**
      * 带点的
+     *
      * @param fileName
      * @return
      */
@@ -269,31 +258,34 @@ public class FtpOperation{
         int pos = fileName.lastIndexOf(".");
         return fileName.substring(pos);
     }
+
     /**
      * 不带点
+     *
      * @param fileName
      * @return
      */
     public static String getNoPointExtention(String fileName) {
         int pos = fileName.lastIndexOf(".");
-        return fileName.substring(pos+1);
+        return fileName.substring(pos + 1);
     }
+
     /**
-     *
      * 功能：根据当前时间获取文件目录
+     *
      * @return String
      */
-    public static String getDateDir(Date dateParam){
+    public static String getDateDir(Date dateParam) {
         Calendar cal = Calendar.getInstance();
-        if(null!=dateParam){
+        if (null != dateParam) {
             cal.setTime(dateParam);
         }
         int currentYear = cal.get(Calendar.YEAR);
         int currentMouth = cal.get(Calendar.MONTH) + 1;
-        int currentDay = cal.get(Calendar.DAY_OF_MONTH) ;
+        int currentDay = cal.get(Calendar.DAY_OF_MONTH);
         //int currentHour = cal.get(Calendar.HOUR_OF_DAY);
         //return currentYear+FtpOperation.DIRSPLIT+currentMouth+FtpOperation.DIRSPLIT+currentDay+FtpOperation.DIRSPLIT+currentHour;
-        return currentYear+FtpOperation.DIRSPLIT+currentMouth+FtpOperation.DIRSPLIT+currentDay;
+        return currentYear + FtpOperation.DIRSPLIT + currentMouth + FtpOperation.DIRSPLIT + currentDay;
     }
 
 
