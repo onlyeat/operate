cae1384fbedd9e5fddd65f4ca9e24f0eeb8de646
onlyeat <195435179@qq.com>
2020-02-13 23:45:36 +0800

    导入微信对账文件

--- guns-main/src/main/java/cn/stylefeng/guns/service/WeiXinFileService.java
+++ guns-main/src/main/java/cn/stylefeng/guns/service/WeiXinFileService.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WeiXinFileService {
+
+    public Boolean importFile(String pathname, String filename) {
+      return true;
+    }
 }
